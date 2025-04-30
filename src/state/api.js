42aa0d5f20ee01cdcb569ed32f6204a8efa14fbb
Muhammad Zunain <muhammadzunain2004@gmail.com>
2025-04-30 14:41:05 +0000

    Backend Url Added

--- src/state/api.js
+++ src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:2000/api/air-monitoring" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://am-backend-two.vercel.app/api" }),
   tagTypes: ["AirData", "AirStats", "AirDataByYear"], 
   endpoints: (builder) => ({
     getAllAirData: builder.query({
